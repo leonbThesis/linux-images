082088f12792e9ffbee779df469eb767cb0de3df
leonbThesis <114205975+leonbThesis@users.noreply.github.com>
2022-09-27 05:55:57 +0200

    Modify bios path and change mac-address

--- i386/run.sh
+++ i386/run.sh
@@ -5,4 +5,4 @@ DIRPATH=$(dirname "$ABSPATH")
 ROOTFS=$1
 KERNEL=$DIRPATH/images/bzImage
 
-qemu-system-i386 -M pc -kernel $KERNEL -drive file=$ROOTFS,if=virtio,format=raw -append "root=/dev/vda console=ttyS0" -net nic,model=e1000 -net user -nographic
+qemu-system-i386 -L "/qemu/pc-bios" -M pc -kernel $KERNEL -drive file=$ROOTFS,if=virtio,format=raw -append "root=/dev/vda console=ttyS0" -net nic,model=e1000,macaddr=54:2A:9C:93:44:B0 -net user -nographic
