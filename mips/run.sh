438bbd03abcaf9e0aecbeb4afb693df49455a224
leonbThesis <114205975+leonbThesis@users.noreply.github.com>
2022-09-27 05:55:32 +0200

    Modify bios path

--- mips/run.sh
+++ mips/run.sh
@@ -5,4 +5,4 @@ DIRPATH=$(dirname "$ABSPATH")
 ROOTFS=$1
 KERNEL=$DIRPATH/images/vmlinux
 
-qemu-system-mips -kernel $KERNEL -hda $ROOTFS -append "root=/dev/hda console=ttyS0" -nographic -device virtio-serial
+qemu-system-mips -L "/qemu/pc-bios" -kernel $KERNEL -hda $ROOTFS -append "root=/dev/hda console=ttyS0" -nographic -device virtio-serial
