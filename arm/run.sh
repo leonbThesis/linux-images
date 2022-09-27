f38ed02be9f12b46c2894dceebac185f04b11e67
leonbThesis <114205975+leonbThesis@users.noreply.github.com>
2022-09-27 05:59:57 +0200

    Modify bios path

--- arm/run.sh
+++ arm/run.sh
@@ -7,4 +7,4 @@ KERNEL=$DIRPATH/images/zImage
 DTB=$DIRPATH/images/vexpress-v2p-ca9.dtb
 QEMU_AUDIO_DRV=none
 
-qemu-system-arm -M vexpress-a9 -smp 1 -m 256 -kernel $KERNEL -dtb $DTB -drive file=$ROOTFS,if=sd,format=raw -append "console=ttyAMA0 root=/dev/mmcblk0" -nographic -net nic,model=lan9118 -net user
+qemu-system-arm -L "/qemu/pc-bios" -M vexpress-a9 -smp 1 -m 256 -kernel $KERNEL -dtb $DTB -drive file=$ROOTFS,if=sd,format=raw -append "console=ttyAMA0 root=/dev/mmcblk0" -nographic -net nic,model=lan9118 -net user
