bf146f20190494e885eb85fcb72482b6eee0317e
leonbThesis <114205975+leonbThesis@users.noreply.github.com>
2022-09-27 05:59:55 +0200

    Modify bios path

--- aarch64/run.sh
+++ aarch64/run.sh
@@ -6,4 +6,4 @@ ROOTFS=$1
 KERNEL=$DIRPATH/images/Image
 QEMU_AUDIO_DRV=none
 
-qemu-system-aarch64 -M virt -cpu cortex-a57 -smp 1 -m 256 -kernel $KERNEL -drive file=$ROOTFS,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -append "console=ttyAMA0 root=/dev/vda" -nographic -netdev user,id=eth0 -device virtio-net-device,netdev=eth0
+qemu-system-aarch64 -L "/qemu/pc-bios" -M virt -cpu cortex-a57 -smp 1 -m 256 -kernel $KERNEL -drive file=$ROOTFS,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -append "console=ttyAMA0 root=/dev/vda" -nographic -netdev user,id=eth0 -device virtio-net-device,netdev=eth0
