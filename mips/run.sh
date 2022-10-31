21ffabdc2a39d7e3e473c3edb62293fe818ba07d
leonbThesis <114205975+leonbThesis@users.noreply.github.com>
2022-10-31 09:06:06 +0100

    Increase RAM size to 2 GiB

--- mips/run.sh
+++ mips/run.sh
@@ -5,4 +5,4 @@ DIRPATH=$(dirname "$ABSPATH")
 ROOTFS=$1
 KERNEL=$DIRPATH/images/vmlinux
 
-qemu-system-mips -L "/qemu/pc-bios" -m 512 -kernel $KERNEL -hda $ROOTFS -append "root=/dev/hda console=ttyS0" -nographic -device virtio-serial
+qemu-system-mips -L "/qemu/pc-bios" -m 2048 -kernel $KERNEL -hda $ROOTFS -append "root=/dev/hda console=ttyS0" -nographic -device virtio-serial
