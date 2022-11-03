2b6bec871ffcdb6a3a1a6375a5154d3b7258b626
leonbThesis <114205975+leonbThesis@users.noreply.github.com>
2022-11-03 08:32:01 +0100

    revert bios data update

--- x86_64/run.sh
+++ x86_64/run.sh
@@ -5,4 +5,4 @@ DIRPATH=$(dirname "$ABSPATH")
 ROOTFS=$1
 KERNEL=$DIRPATH/images/bzImage
 
-qemu-system-x86_64 -L "/qemu/pc-bios" -m 2048 -M pc -kernel $KERNEL -drive file=$ROOTFS,if=virtio,format=raw -rtc base="2013-10-15",clock=vm  -append "root=/dev/vda console=ttyS0" -net nic,model=e1000,macaddr=54:2A:9C:93:44:B0 -net user -nographic
+qemu-system-x86_64 -L "/qemu/pc-bios" -m 2048 -M pc -kernel $KERNEL -drive file=$ROOTFS,if=virtio,format=raw  -append "root=/dev/vda console=ttyS0" -net nic,model=e1000,macaddr=54:2A:9C:93:44:B0 -net user -nographic
